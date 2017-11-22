ff599234a4fe87e477a88d34bba13e787038fb33
luckyzd <zhangdi5@xiaomi.com>
2017-11-22 19:51:03 +0800

    save20171122

--- shell/t.sh
+++ shell/t.sh
@@ -8,9 +8,32 @@
 # date "+%Y-%m-%d %H:%M:%S"
 # 当前分支名称
 # git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3
-DATE=$(+%Y-%m-%d %H:%M:%S)
-git status
-git add --all
-git commit -m "zxd $DATE"
-git pull
-git checkout -b "dev$(%Y%m%d%H%M%S)"
+# datetime=$(date "+%Y-%m-%d %H:%M:%S")
+datetime=$(date "+%Y%m%d %H:%M:%S")
+# git status
+# git add --all
+# git commit -m "save"
+# git pull
+# git push
+# git checkout -b "dev$(date +%Y%m%d)"
+
+# ""字符串！
+# status="git status"
+# ``执行命令
+status=`git status`
+
+# >> ；如果文件不存在，将创建新的文件，并将数据送至此文件；如果文件存在，则将数据添加在文件后面
+# >  ；如果文件不存在，同上，如果文件存在，先将文件清空，然后将数据填入此文件
+# echo $status > log.txt
+# open log.txt
+
+if [[ $status =~ "git add" ]]; then
+  git add --all
+  git commit -m "save$(date +%Y%m%d)"
+  git pull
+  git push
+  echo 'push ok'
+else 
+  echo '!need git add'
+fi
+
